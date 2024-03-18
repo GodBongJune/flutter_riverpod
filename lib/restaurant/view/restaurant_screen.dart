527fb30c7724491dfbca01d14f079c22630e0b58
bong <darksen7693@gmail.com>
2024-03-18 15:17:38 +0900

    fromJson 생성자

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -41,20 +41,8 @@ class RestaurantScreen extends StatelessWidget {
                 itemCount: snapshot.data!.length,
                 itemBuilder: (_, index) {
                   final item = snapshot.data![index];
-
-                  //parse
-                  final pItem = RestaurantModel(
-                    id: item["id"],
-                    name: item["name"],
-                    thumbUrl: "http://$ip${item["thumbUrl"]}",
-                    tags: List<String>.from(item["tags"]),
-                    priceRange: RestaurantPriceRange.values.firstWhere(
-                      (e) => e.name == item["priceRange"],
-                    ),
-                    ratings: item["ratings"],
-                    ratingsCount: item["ratingsCount"],
-                    deliveryTime: item["deliveryTime"],
-                    deliveryFee: item["deliveryFee"],
+                  final pItem = RestaurantModel.fromJson(
+                    json: item,
                   );
 
                   return RestaurantCard(
