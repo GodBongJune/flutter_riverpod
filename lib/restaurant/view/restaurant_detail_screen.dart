4f90796efe77d597e67b26a6183e55badf608a06
bong <darksen7693@gmail.com>
2024-03-18 16:34:24 +0900

    SliverList구현

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -10,24 +10,80 @@ class RestaurantDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: "불타는 떡볶이",
-      child: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset("assets/img/food/ddeok_bok_gi.jpg"),
-            name: "불타는 떡볶이",
-            tags: ["떡볶이", "맛있음", "치즈"],
-            ratingsCount: 100,
-            deliveryTime: 30,
-            deliveryFee: 3000,
-            ratings: 4.76,
-            isDetail: true,
-            detail: "맛있는 떡볶이",
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ProductCard(),
-          ),
+      child: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
         ],
+      ),
+
+      // Column(
+      //   children: [
+      //     RestaurantCard(
+      //       image: Image.asset("assets/img/food/ddeok_bok_gi.jpg"),
+      //       name: "불타는 떡볶이",
+      //       tags: ["떡볶이", "맛있음", "치즈"],
+      //       ratingsCount: 100,
+      //       deliveryTime: 30,
+      //       deliveryFee: 3000,
+      //       ratings: 4.76,
+      //       isDetail: true,
+      //       detail: "맛있는 떡볶이",
+      //     ),
+      //     Padding(
+      //       padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      //       child: ProductCard(),
+      //     ),
+      //   ],
+      // )
+    );
+  }
+
+  SliverPadding renderLabel() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          "메뉴",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  renderProducts() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ProductCard(),
+            );
+          },
+          childCount: 10,
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset("assets/img/food/ddeok_bok_gi.jpg"),
+        name: "불타는 떡볶이",
+        tags: ["떡볶이", "맛있음", "치즈"],
+        ratingsCount: 100,
+        deliveryTime: 30,
+        deliveryFee: 3000,
+        ratings: 4.76,
+        isDetail: true,
+        detail: "맛있는 떡볶이",
       ),
     );
   }
