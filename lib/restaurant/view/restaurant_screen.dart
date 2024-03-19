81267f991a18a5c55cd5530e4404587e531e37ce
bong <darksen7693@gmail.com>
2024-03-19 15:10:09 +0900

    RestaurantModel JsonSerializable 적용

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -44,7 +44,7 @@ class RestaurantScreen extends StatelessWidget {
                 itemCount: snapshot.data!.length,
                 itemBuilder: (_, index) {
                   final item = snapshot.data![index];
-                  final pItem = RestaurantModel.fromJson(json: item);
+                  final pItem = RestaurantModel.fromJson(item);
 
                   return GestureDetector(
                       onTap: () {
